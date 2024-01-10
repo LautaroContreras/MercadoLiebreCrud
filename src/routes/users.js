11f419b352d90f223072ddb382c61143dd879e40
LautaroContreras <LautaroContreras1998@gmail.com>
2024-01-10 19:54:49 -0300

    Arreglando errores

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,10 +5,10 @@ const usersController = require('../controllers/usersControllers')
 const { check } = require('express-validator');
 
 const validateRegister =[
-    check("name").notEmpty().withMessage('Debes completar el nombre').bail().isLength('{min : 5}').withMessage('El nombre debe tener al menos 5 caracteres'),
-    check("lastName").notEmpty().withMessage('Debes completar el apellido').bail().isLength('{min : 5}').withMessage('El apellido debe tener al menos 5 caracteres'),
+    check("name").notEmpty().withMessage('Debes completar el nombre').bail().isLength({min : 5}).withMessage('El nombre debe tener al menos 5 caracteres'),
+    check("lastName").notEmpty().withMessage('Debes completar el apellido').bail().isLength({min : 5}).withMessage('El apellido debe tener al menos 5 caracteres'),
     check("email").notEmpty().withMessage('Debes completar el email').bail().isEmail().withMessage('Debes ingresar un email valido'),
-    check("password").notEmpty().withMessage('Debes completar la contraseña').bail().isLength('{min : 5}').withMessage('La contraseña debe tener al menos 5 caracteres'),
+    check("password").notEmpty().withMessage('Debes completar la contraseña').bail().isLength({min : 5}).withMessage('La contraseña debe tener al menos 5 caracteres'),
 ];
 
 /*** CREATE ONE USER***/ 
